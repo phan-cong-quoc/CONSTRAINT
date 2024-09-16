ec566648be6cab317f37a60a9f3312dee6904fb8
phancongquoc <phanquoc870@gmail.com>
2024-09-17 02:36:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/constraint/Login.java
@@ -0,0 +1,55 @@
+package com.example.constraint;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+ Button btnlogin;
+ TextView login_Username,login_Password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        TextView    btn=findViewById(R.id.textviewSignup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this,Register.class));
+            }
+        });
+        //hiển thị khi nhấp vào login
+
+        login_Username = findViewById(R.id.login_Username);
+        login_Password = findViewById(R.id.login_Password);
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                login_Username.setText("Phan Công Quốc");
+                login_Password.setText("22115053122134");
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/constraint/Register.java
@@ -0,0 +1,52 @@
+package com.example.constraint;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Register extends AppCompatActivity {
+    Button btn_Register;
+    TextView login_Username,inputEmail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+
+        TextView btn=findViewById(R.id.alreadyHaveAccount);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Register.this,Login.class));
+            }
+        });
+
+        //
+        login_Username = findViewById(R.id.login_Username);
+        inputEmail = findViewById(R.id.inputEmail);
+        btn_Register = findViewById(R.id.btn_Register);
+
+        btn_Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                login_Username.setText("Phan Công Quốc");
+                inputEmail.setText("22115053122134");
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
